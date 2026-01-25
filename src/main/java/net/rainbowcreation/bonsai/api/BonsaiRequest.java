b1ddaad9c835d27dcac1bddbca18369251d13579
NathanielSong <ampnattahapat@gmail.com>
2026-01-25 18:44:37 +0700

    tmp fix missing Req/Res

---
+++ src/main/java/net/rainbowcreation/bonsai/api/BonsaiRequest.java
@@ -0,0 +1,17 @@
+package net.rainbowcreation.bonsai.api;
+
+public class BonsaiRequest {
+    public final String op;
+    public final String db;
+    public final String table;
+    public final String key;
+    public final byte[] payload;
+
+    public BonsaiRequest(String op, String db, String table, String key, byte[] payload) {
+        this.op = op;
+        this.db = db;
+        this.table = table;
+        this.key = key;
+        this.payload = payload;
+    }
+}
---
+++ src/main/java/net/rainbowcreation/bonsai/api/BonsaiResponse.java
@@ -0,0 +1,23 @@
+package net.rainbowcreation.bonsai.api;
+
+public class BonsaiResponse {
+    public int status;
+    public byte[] body;
+
+    public BonsaiResponse(int status, byte[] body) {
+        this.status = status;
+        this.body = body;
+    }
+
+    public static BonsaiResponse ok() {
+        return new BonsaiResponse(200, null);
+    }
+
+    public static BonsaiResponse ok(byte[] body) {
+        return new BonsaiResponse(200, body);
+    }
+
+    public static BonsaiResponse error(int code, String msg) {
+        return new BonsaiResponse(code, msg.getBytes());
+    }
+}
